dbf8fce8b6d87bf985b6231e1f396f1cf9923add
Franco Suppa <suppa.f.n@gmail.com>
2022-01-25 14:37:56 -0300

    JSX Transpilling

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+      <div className='navBar'>
+          <ul className="navList">
+          <h1>Massimo</h1>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Remeras</a></li>
+          <li><a href="#">Pantalones</a></li>
+          </ul></div>
+    );
+  }
+
+export default NavBar;
